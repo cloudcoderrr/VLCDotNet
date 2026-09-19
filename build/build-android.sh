32c3b47b115650ef40e9b79fbb4bd9f728a53491
cloudcoderrr <201903827+cloudcoderrr@users.noreply.github.com>
2026-09-19 13:52:29 -0700

    Fix Android OpenSLES and libvpx build blockers

--- build/build-android.sh
+++ build/build-android.sh
@@ -55,7 +55,7 @@ CONTRIB_BUILD="${VLC_SRC}/contrib/contrib-android-${ARCH}"
 mkdir -p "${CONTRIB_BUILD}"
 (
   cd "${CONTRIB_BUILD}"
-  env "${CONTRIB_ENV[@]}" ../bootstrap --host="${TRIPLET}" --disable-disc --disable-x264 --disable-x265 --disable-mpg123 --disable-protobuf --disable-xcb --disable-goom --disable-sidplay2
+  env "${CONTRIB_ENV[@]}" ../bootstrap --host="${TRIPLET}" --disable-disc --disable-x264 --disable-x265 --disable-mpg123 --disable-protobuf --disable-xcb --disable-goom --disable-sidplay2 --disable-vpx
   if ! env "${CONTRIB_ENV[@]}" make prebuilt 2>/dev/null; then
     warn "Prebuilt contribs unavailable for ${TRIPLET}; building from source"
     env "${CONTRIB_ENV[@]}" make -j"$(jobs)" fetch
@@ -75,6 +75,7 @@ CONFIG_FLAGS=(
   "--with-contrib=${VLC_SRC}/contrib/${TRIPLET}"
   --disable-vlc --disable-qt --disable-skins2 --disable-nls
   --disable-lua --disable-a52 --disable-sid
+  --disable-vpx
   --disable-xcb --disable-alsa --disable-pulse --disable-vdpau
   --disable-v4l2 --disable-vnc --disable-gnutls --disable-srt
   --enable-avcodec --enable-swscale
