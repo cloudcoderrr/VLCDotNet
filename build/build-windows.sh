1526502f2edc73534d795803566b87b5bb1e67c0
cloudcoderrr <201903827+cloudcoderrr@users.noreply.github.com>
2026-09-20 08:06:07 -0700

    Fix Windows update-check flag; exclude iOS-GLES vout on Catalyst
    windows: the update checker is --disable-update-check (hyphenated); the win32 configure.sh force-enables it and it hard-requires libgcrypt we do not build. maccatalyst: patch 0016 adds a HAVE_MACCATALYST configure conditional and skips the OpenGLES-based iOS vout modules (vout_ios/glconv_cvpx) that Catalyst cannot build (Metal only).

--- build/build-windows.sh
+++ build/build-windows.sh
@@ -53,7 +53,7 @@ BUILD_ARGS=(-r -z -u -S 0x0A000000 -a "${ARCH}")
 # goom, mpc, schroedinger, shout, theora, zvbi, live555, ...). We only build the
 # minimal contrib closure, so override each with a trailing --disable-* (autoconf
 # takes the last flag) and also drop the default-required optical-disc modules.
-export CONFIGFLAGS="${CONFIGFLAGS:-} --disable-lua --disable-live555 --disable-realrtsp --disable-faad --disable-flac --disable-goom --disable-libcddb --disable-mpc --disable-schroedinger --disable-shout --disable-theora --disable-zvbi --disable-dvdread --disable-dvdnav --disable-nls --disable-update --disable-bluray --disable-gnutls --disable-srt --disable-aribcam"
+export CONFIGFLAGS="${CONFIGFLAGS:-} --disable-lua --disable-live555 --disable-realrtsp --disable-faad --disable-flac --disable-goom --disable-libcddb --disable-mpc --disable-schroedinger --disable-shout --disable-theora --disable-zvbi --disable-dvdread --disable-dvdnav --disable-nls --disable-update-check --disable-bluray --disable-gnutls --disable-srt --disable-aribcam"
 export CONTRIBFLAGS="${CONTRIBFLAGS:-} --disable-all --enable-ffmpeg --enable-opus --enable-ass --enable-matroska --disable-net --disable-sout --disable-disc"
 EXTRA_LINK_FLAGS=""
 if [ "${ARCH}" = "x86_64" ]; then
